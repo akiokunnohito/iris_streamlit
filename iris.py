81ac9754cedf57be0de8a267625d8d5217998a5c
akio kawahara <akawahara.qu@gmailcom>
2023-01-15 18:30:30 +0900

    1st commit

---
+++ iris.py
@@ -0,0 +1,66 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+from sklearn.linear_model import LogisticRegression
+from sklearn.datasets import load_iris
+
+iris = load_iris()
+df = pd.DataFrame(iris.data, columns=iris.feature_names)
+
+# 目標値
+df["target"] = iris.target
+
+# 目標値を数字から花の名前に変更
+df.loc[df['target'] == 0, "target"] = "setosa"
+df.loc[df['target'] == 1, "target"] = "versicolor"
+df.loc[df['target'] == 2, "target"] = "verginica"
+
+# データの分割
+x = iris.data[:, [0, 2]]
+t = iris.target
+
+# ロジスティック回帰
+log_reg = LogisticRegression()
+log_reg.fit(x, t)
+
+# サイドバー（入力画面）
+st.sidebar.header("input features")
+
+sepal_value = st.sidebar.slider("sepal length(cm)",
+                                min_value = 0.0,
+                                max_value = 10.0, step=0.1)
+petal_value = st.sidebar.slider("petal length(cm)",
+                                min_value = 0.0,
+                                max_value = 10.0, step=0.1)
+
+# メインパネル
+st.title("Iris Classifier")
+st.write("## input value")
+
+# Inputデータ
+value_df = pd.DataFrame([], columns=[
+    "data", "sepal length(cm)", "petal length(cm)"
+    ], )
+record = pd.Series(["data", sepal_value, petal_value],
+                    index = value_df.columns)
+value_df = value_df.append(record, ignore_index=True)
+value_df.set_index("data", inplace=True)
+
+# 入力値の値
+st.write(value_df)
+
+# 予測値のデータ振れ0務
+pred_probs = log_reg.predict_proba(value_df)
+pred_df = pd.DataFrame(
+    pred_probs, 
+    columns=["setosa", "versicolor", "virginica"],
+    index=["probability"]
+    )
+
+st.write("## prediction")
+st.write(pred_df)
+
+# 予測結果の出力
+name = pred_df.idxmax(axis=1).tolist()
+st.write("## Result")
+st.write("このアイリスはきっと", str(name[0]),"です。")
---
+++ main.py
@@ -0,0 +1,92 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+st.title("streamlit 基礎")
+st.write("Hello World!")
+
+# データフレームの準備
+df = pd.DataFrame({
+    "1列目": [1, 2, 3, 4, 5],
+    "2列目": [10, 20, 30, 40, 50]
+})
+
+# 動的なテーブル
+# st.dataframe(df)
+
+# 引数を使用した動的テーブル
+# st.dataframe(
+#     df.style.highlight_max(axis=0), 
+#     width = 100,
+#     height=150
+#     )
+
+# 静的なテーブル
+# st.table(df)
+
+# 10行3列のデータフレームを準備
+df = pd.DataFrame(
+    np.random.rand(10, 3),
+    columns=["a", "b", "c"]
+)
+
+# 折れ線グラフ
+st.line_chart(df)
+
+# 面グラフ
+st.area_chart(df)
+
+# 棒グラフ
+st.bar_chart(df)
+
+# プロットする乱数をデータフレームで用意
+df_map = pd.DataFrame(
+    np.random.rand(100,2) / [10, 10] + [35.69, 139.70],
+    columns = ["latitude", "longitude"]
+)
+
+# マップをプロット
+st.map(df_map)
+
+# Pillow 画像の読み込み
+from PIL import Image
+
+# 画像の読み込み
+# img = Image.open("iris.jpg")
+# st.image(img,caption = "Iris", use_column_width=True)
+
+# チェックボックス
+if st.checkbox("show Image"):
+    img = Image.open("iris.jpg")
+    st.image(img,caption = "Iris", use_column_width=True)
+
+# セレクトボックス
+option = st.selectbox(
+    "好きな数字を入力してください。",
+    list(range(1, 11))
+)
+
+# テキスト入力による値の動的変更
+text = st.text_input("あなたの好きなスポーツを教えてください")
+"あなたの好きなスポーツ：", text
+
+# スライダーによる値の動的変更
+condition = st.slider("あなたの今の調子は？", 0, 100, 25)
+"コンディション:", condition
+
+"あなたの好きな数字は", option, "です。"
+"""
+# unchiunchi
+## unchiunchi
+### unchiunchi
+#### unchiunchi
+- unchiunchi
+- unchiunchi
+1. unchiunchi
+2. unchiunchi
+3. unchiunchi
+
+> unchiunchi
+
+`Hello, unchi!`
+"""
